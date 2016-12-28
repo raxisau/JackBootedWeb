2542c593d1f3d8b0e87c9a7a039e21d96277bbfe
brett@brettdutton.com <brett@192.168.5.131>
2016-12-28 16:27:53 +1000

    Updated copyright message

--- vendor/jackbooted/forms/PipeLine.php
+++ vendor/jackbooted/forms/PipeLine.php
@@ -3,7 +3,7 @@ namespace Jackbooted\Forms;
 
 use \Jackbooted\Config\Cfg;
 /**
- * @copyright Confidential and copyright (c) 2016 Jackbooted Software. All rights reserved.
+ * @copyright Confidential and copyright (c) 2017 Jackbooted Software. All rights reserved.
  *
  * Written by Brett Dutton of Jackbooted Software
  * brett at brettdutton dot com
