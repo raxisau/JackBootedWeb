0ef5d2c61243336e9b35c040c73c25d02367cdea
Brett Dutton <github@brettdutton.com>
2017-06-25 21:49:50 +1000

    test callback

--- callback.php
+++ callback.php
@@ -1,9 +1,18 @@
 <?php
+$config = [];
+require_once( dirname( __FILE__ ) . '/config.env.php' );
 
+if ( ! isset( $_SERVER['HTTP_X_HUB_SIGNATURE'] ) ) exit;
+
+list( $algo, $hash ) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+$rawPost = file_get_contents( 'php://input' );
+if ( $hash !== hash_hmac( $algo, file_get_contents('php://input'), $config['githubsecret'] ) ) exit;
+    
 $Name      = "Push Callback"; //senders name 
 $email     = "github@brettdutton.com"; //senders e-mail adress 
 $recipient = "brett@brettdutton.com"; //recipient 
-$mail_body = system( 'cd ' . dirname( __FILE__ ) . '; /usr/bin/git pull' ); //mail body 
+$mail_body = $_SERVER['HTTP_X_HUB_SIGNATURE'] . "\n" . 
+             system( 'cd ' . dirname( __FILE__ ) . '; /usr/bin/git pull' ); //mail body 
 $subject   = "Jack Commit " . date( "Y-m-d H:i:s" ); //subject 
 $header    = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields 
 
