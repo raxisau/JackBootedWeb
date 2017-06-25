9b2f2fd9000eae3e9446a8b6f07d0115b2173691
Brett Dutton <github@brettdutton.com>
2017-06-25 21:28:02 +1000

    add callback

---
+++ callback.php
@@ -0,0 +1,10 @@
+<?php
+
+$Name      = "Commit Callback"; //senders name 
+$email     = "github@brettdutton.com"; //senders e-mail adress 
+$recipient = "brett@brettdutton.com"; //recipient 
+$mail_body = system( 'cd ' . dirname( __FILE__ ) . '; /usr/bin/git pull' ); //mail body 
+$subject   = "Jack Commit " . date( "Y-m-d H:i:s" ); //subject 
+$header    = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields 
+
+mail($recipient, $subject, $mail_body, $header); //mail command :) 
