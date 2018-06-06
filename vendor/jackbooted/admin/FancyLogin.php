fd65a97e38903c390d7c4dbecc9248104b20b957
Brett Dutton <brett@brettdutton.com>
2018-06-06 14:30:51 +1000

    Updated the timezone

--- vendor/jackbooted/admin/FancyLogin.php
+++ vendor/jackbooted/admin/FancyLogin.php
@@ -45,7 +45,7 @@ class FancyLogin extends Login {
     public function index ( ) {
         $html = ( G::isLoggedIn () ) ? $this->displayUserDetails() : $this->loginForm();
 
-        $html .= '<b>Timezone: ' . self::get ( 'timezone', false ) . '</b><br/>';
+        $html .= '<b>Timezone: ' . Cfg::get ( 'timezone', false ) . '</b><br/>';
 
         return $html;
     }
