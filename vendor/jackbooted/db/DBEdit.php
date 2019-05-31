f7beecfd8f517e2e283fccfb415dcddd75984d93
Brett Dutton <brett@brettdutton.com>
2019-05-31 14:44:43 +0700

    added tinymce

--- vendor/jackbooted/db/DBEdit.php
+++ vendor/jackbooted/db/DBEdit.php
@@ -260,7 +260,7 @@ class DBEdit extends \Jackbooted\Util\JB {
 
         if ( !in_array( $type, [ self::HIDDEN, self::NONE ] ) ) {
             $html = Tag::tr() .
-                      Tag::td() . $this->convertColumnToTitle( $colName ) . Tag::_td() .
+                      Tag::th( [ 'valign' => 'top' ] ) . $this->convertColumnToTitle( $colName ) . Tag::_th() .
                       Tag::td() . $html .Tag::_td() .
                     Tag::_tr();
         }
