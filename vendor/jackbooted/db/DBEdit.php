5b9d9cd2c89d9225013fa4a8e34f2918d00bfeb2
Brett Dutton <github@brettdutton.com>
2020-08-27 12:07:46 +0700

    Added a color picker

--- vendor/jackbooted/db/DBEdit.php
+++ vendor/jackbooted/db/DBEdit.php
@@ -409,7 +409,7 @@ class DBEdit extends \Jackbooted\Util\JB {
         $id = 'colorPicker_' . Invocation::next();
         $exampleStyle = '';
 
-        $html = "<select name=\"$name\" onchange=\"$('{$id}').attr('style',$(this).attr('style'))\">";
+        $html = "<select name=\"$name\" onchange=\"$('#{$id}').attr('style',$(this).attr('style'))\">";
         foreach ( $colorList as $colItem ) {
             $colName = $colItem[0] . "|" . $colItem[1];
             if ( $colName == $default ) {
