10ebac3206ffac41817b921c1bc3a0e17d852a40
Brett (AA.ai) Dutton <brett@addaxis.ai>
2022-01-10 10:55:12 +0700

    Used the fonts

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ $pageTimer = new \Jackbooted\Time\Stopwatch( "Page Load" );
         <?php /* <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" > */ ?>
         <?php /* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"> */ ?>
         <?= JS::libraryWithDependancies( JS::JQUERY ); ?>
-        <?= JS::library( 'fontawesome-all.min.css' ); ?>
     </head>
     <body>
         <table width="100%" cellpadding="5" cellspacing="0" border="1">
