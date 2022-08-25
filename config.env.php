a119b38236671c3457e52e556ad6f0cf2d3e7c2d
Brett (AA.ai) Dutton <brett@addaxis.ai>
2022-08-25 08:16:34 +0700

    Added config

---
+++ config.env.php
@@ -0,0 +1,12 @@
+<?php
+if ( ! isset( $_SERVER['HTTPS'] ) || $_SERVER['HTTPS'] != 'on' ) {
+    $config['site_url']     = 'http://' . $config['server'];
+}
+else {
+    $config['site_url']     = 'https://' . $config['server'];
+}
+$config['js_url']       = $config['site_url'] . '/js';
+$config['images_url']   = $config['site_url'] . '/images';
+$config['favicon']      = $config['site_url'] . '/favicon.ico';
+$config['githubsecret'] = 'Secret';
+
